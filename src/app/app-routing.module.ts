37ad97c8fda61b11428ff6e03c23102d3f22f96e
Arkaitz Vizuete <personal@arkaitzvizuete.com>
2021-06-14 10:50:45 +0200

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './modules/about/about.component';
+import { ContactComponent } from './modules/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AboutComponent},
+  {path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
